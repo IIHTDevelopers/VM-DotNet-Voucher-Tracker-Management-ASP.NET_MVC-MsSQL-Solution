2730cb06c6a2436bd64bd4ab1222a20b34753199
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 11:34:07 +0530

    Add files via upload

---
+++ VoucherTrackerApp/DAL/Repository/VoucherTrackerRepository.cs
@@ -0,0 +1,64 @@
+﻿using VoucherTrackerApp.DAL.Interface;
+using VoucherTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VoucherTrackerApp.DAL.Repository
+{
+    public class VoucherTrackerRepository : IVoucherTrackerRepository
+    {
+        private VoucherTrackerDbContext _context;
+        public VoucherTrackerRepository(VoucherTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Voucher> GetVouchers()
+        {
+             return _context.Vouchers.ToList();
+        }
+        public Voucher GetVoucherByID(int id)
+        {
+            return _context.Vouchers.Find(id);
+        }
+        public Voucher InsertVoucher(Voucher voucher)
+        {
+            return _context.Vouchers.Add(voucher);
+        }
+        public int DeleteVoucher(int voucherID)
+        {
+            Voucher voucher = _context.Vouchers.Find(voucherID);
+            var res= _context.Vouchers.Remove(voucher);
+            return res.VoucherId;
+        }
+        public bool UpdateVoucher(Voucher voucher)
+        {
+            var res= _context.Entry(voucher).State = EntityState.Modified;
+            return res.Equals("voucher");
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ VoucherTrackerApp/DAL/Repository/VoucherTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using VoucherTrackerApp.DAL.Interface;
+using VoucherTrackerApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VoucherTrackerApp.DAL.Repository
+{
+    public class VoucherTrackerService : IVoucherTrackerInterface
+    {
+        private IVoucherTrackerRepository _repo;
+        public VoucherTrackerService(IVoucherTrackerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public int DeleteVoucher(int voucherId)
+        {
+            var res= _repo.DeleteVoucher(voucherId);
+            return res;
+        }
+
+        public Voucher GetVoucherByID(int voucherId)
+        {
+            return _repo.GetVoucherByID(voucherId);
+        }
+        public void Save()
+        {
+            _repo.Save();
+        }
+
+
+        IEnumerable<Voucher> IVoucherTrackerInterface.GetVouchers()
+        {
+            return _repo.GetVouchers();
+        }
+
+        Voucher IVoucherTrackerInterface.InsertVoucher(Voucher voucher)
+        {
+            return _repo.InsertVoucher(voucher);
+        }
+
+        bool IVoucherTrackerInterface.UpdateVoucher(Voucher voucher)
+        {
+            return _repo.UpdateVoucher(voucher);
+        }
+    }
+}
